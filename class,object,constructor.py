de518f049099b43a5e165743419e82be92b9838c
Guru pradeep <gurupradeepc@gmail.com>
2026-08-28 12:15:48 +0530

    c

--- class,object,constructor.py
+++ class,object,constructor.py
@@ -142,40 +142,87 @@ acc2.display()
 
 #2
 
-class Student():
-    college="ABC College"
-    def __init__(self,name,rollno,marks):
-        self.name=name
-        self.rollno=rollno
-        if(marks >=0 and marks <=100):
-            self.mraks=marks
-        else:
-            self.marks=0
-s1=Student("Alice",123,34)
-s2=Student("Bob",124,-94)
-s3=Student("Alice",125,10)
-print(s1.__dict__)
-print(s2.__dict__)
-print(s3.__dict__)
+# class Student():
+#     college="ABC College"
+#     def __init__(self,name,rollno,marks):
+#         self.name=name
+#         self.rollno=rollno
+#         if(marks >=0 and marks <=100):
+#             self.mraks=marks
+#         else:
+#             self.marks=0
+# s1=Student("Alice",123,34)
+# s2=Student("Bob",124,-94)
+# s3=Student("Alice",125,10)
+# print(s1.__dict__)
+# print(s2.__dict__)
+# print(s3.__dict__)
 
 #3
-class Product():
+# class Product():
+#     products=[]
+#     store_name = "ABC Store"
+#     def __init__(self,name,price,quantity):
+#         self.name=name
+#         if(price > 0 and quantity>0):
+#             self.price=price
+#             self.quantity=quantity
+#         else:
+#             print("Invalid price or quantity")
+#         Product.products.append(name)
+# p1=Product("Laptop",10000,2)
+# p2=Product("Mouse",100,-2)
+# p3=Product("Ps5",10000,2)
+# print(p1.__dict__)
+# print(p2.__dict__)
+# print(p3.__dict__)
+# print(Product.products)
 
-    store_name = "ABC Store"
-    def __init__(self,name,price,quantity):
+#A company wants to generate basic salary information when employee objects are created.
+# Create a class Employee with class variables company = "TechCorp" and employee_count = 0.
+# The constructor should accept name, department, salary, and experience.
+# Validate that salary and experience are not negative.
+# Based on experience, calculate a bonus inside the constructor: employees with more than 5 years receive 15%,
+# employees with 3–5 years receive 10%, and employees with less than 3 years receive 5%.
+# Create an instance dictionary pay_details containing the employee’s name, salary, experience, bonus, and final salary.
+# Generate an employee ID using employee_count. Create three employee objects and display their _dict_.
+
+class Employee():
+    company="TechCorp"
+    employee_count=0
+    def __init__(self,name,department,salary,experience):
         self.name=name
-        if(price > 0 and quantity>0):
-            self.price=price
-            self.quantity=quantity
+        self.department=department
+
+        if(salary>0 and experience>0):
+            self.salary = salary
+            self.experience = experience
+            if (experience > 5):
+                bonus = (salary * 0.15)
+            elif (experience >= 3 and experience <= 5):
+                bonus = (salary * 0.1)
+            else:
+                bonus = (salary * 0.05)
+            final_salary = salary + bonus
+            Employee.employee_count += 1
+            self.employee_id = self.employee_count
+
+            self.pay_details = {
+                "employee;s name": name,
+                "salary": salary,
+                "experience": experience,
+                "bonus": bonus,
+                "final_salary": final_salary
+            }
         else:
-            print("Invalid price or quantity")
-        Product.products.append(name)
-p1=Product("Laptop",10000,2)
-p2=Product("Mouse",100,-2)
-p3=Product("Ps5",10000,2)
-print(p1.__dict__)
-print(p2.__dict__)
-print(p3.__dict__)
-print(Product.products)
+            print("Invalid salary or experience")
 
-
+emp1=Employee("pradeep","Developer",1000000,6)
+emp2=Employee("pavan","Testing",70000,2)
+emp3=Employee("balaji","sales",60000,4)
+# print(emp1.__dict__)
+# print(emp2.__dict__)
+# print(emp3.__dict__)
+print(emp1.pay_details)
+print(emp2.pay_details)
+print(emp3.pay_details)
