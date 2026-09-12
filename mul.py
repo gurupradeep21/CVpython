969270b71afadae899e424af454b35291e5b8179
Guru pradeep <gurupradeepc@gmail.com>
2026-09-12 23:40:53 +0530

    c3

--- mul.py
+++ mul.py
@@ -22,7 +22,7 @@ print()
 # print(full_name("Guru","Pradeep","Yadav"))
 
 
-#    --------------------------------------------
+#    -------------------------------------
  # create a fuc calculate bill with parameters price and quantity that returns toatal cost. add 40rs delivery if toatal is less than 200, call in a single line and print it
  # create a python application with 3 functions 1 total with three subject marks. 2nd one average which takes inputnof total marks and avg. 3rd one takes avg as input if avg is > 85 return A grade, 75 b, bet 65- 75 return c50-65 return d.
 
