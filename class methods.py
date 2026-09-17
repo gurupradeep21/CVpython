d8513ebcc5af1f7c47230687fa13be2a45861007
Guru pradeep <gurupradeepc@gmail.com>
2026-09-17 23:02:29 +0530

    c5

--- class methods.py
+++ class methods.py
@@ -1,6 +1,40 @@
-class PasswordUtils:
+# class PasswordUtils:
+#     @staticmethod
+#     def is_strong(password):
+#         if len(password)>=8:
+#             return True
+#     def generate_hint(self):
+
+class Student:
+    count=0
+    def __init__(self,name,rollno,marks):
+        self.name=name
+        self.rollno=rollno
+        self.marks=marks
+        Student.count+=1
+
+    def display(self):
+        print("Student Name:",self.name)
+        print("Student Roll No:",self.rollno)
+        print("Student Marks:",self.marks)
+        print("Student result:",Student.check(self.marks))
+
+    def calculate_percentage(self):
+        per = (sum(self.marks) // len(self.marks))*100
+        print(self.name,"has secured",per,"%")
+
+    @classmethod
+    def total(cls):
+        print("Total Students enrolled :",cls.count)
+
     @staticmethod
-    def is_strong(password):
-        if len(password)>=8:
-            return True
-    def generate_hint(self):
+    def check(marks):
+        if sum(marks) >= 40:
+            print("Pass")
+        else:
+            print("Fail")
+
+s1=Student("A",1,[60,70,80])
+s2=Student("A",1,[60,70,80])
+s3=Student("A",1,[60,70,80])
+s1.display(),
---
+++ iterators.py
@@ -0,0 +1,125 @@
+# l=[1,2,3,4,5]
+# for i in l:                   #list is iterable
+#     print(i)
+
+# l=[1,2,3,4,5]
+# it=iter(l)
+# print(it.__next__())
+# print(it.__next__())
+# print(it.__next__())
+# print(it.__next__())
+# print(it.__next__())
+
+class A:
+    def __init__(self):
+        self.val=1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if(self.val<=5):
+            num=self.val
+            self.val+=1
+            return num
+        else:
+            raise StopIteration
+obj=A()
+# for i in obj:
+#     print(i)
+
+
+print(obj.__next__())
+print(obj.__next__())
+print(obj.__next__())    # iterators are for only single use only
+print(obj.__next__())
+print(obj.__next__())
+l = [1,2,3,4,5]
+# for i in l:
+#     print(i)
+
+# it = iter(l)
+# it = l.__iter__()
+# print(it)
+
+# print(it.__next__())
+# print(it.__next__())
+# print(it.__next__())
+# print(it.__next__())
+# print(it.__next__())
+---------------------------------------------------------
+class A:
+    def __init__(self):
+        self.val = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.val <= 5:
+            num = self.val
+            self.val += 1
+            return num
+        else:
+            raise StopIteration
+
+# obj = A()
+# for i in obj:
+#     print(i)
+
+# print(obj.__next__())
+# print(obj.__next__())
+# print(obj.__next__())
+# print(obj.__next__())
+# print(obj.__next__())
+# print(obj.__next__())
+# obj2 = A()
+# print(obj2.__next__())
+# print(next(obj2))
+# l = list(range(1, 11))
+# print(l)
+class Numbers:
+    def __init__(self, start, end):
+        self.val = start
+        self.end = end
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.val <= self.end:
+            current = self.val
+            self.val += 1
+            return current
+        else:
+            raise StopIteration
+five_to_ten = Numbers(5, 10)
+# for i in five_to_ten:
+#     print(i)
+
+twenty_to_hundred = Numbers(20, 100)
+# for i in twenty_to_hundred:
+#     print(i)
+
+class Fibonacci:
+    def __init__(self, n):
+        self.count = 1
+        self.n = n
+        self.a , self.b = 0, 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count <= self.n:
+            val = self.a
+            self.a, self.b = self.b, self.a + self.b
+            self.count += 1
+            return val
+        else:
+            raise StopIteration
+five = Fibonacci(5)
+# for i in five:
+#     print(i)
+#
+ten = Fibonacci(10)
+for i in ten:
+    print(i)
